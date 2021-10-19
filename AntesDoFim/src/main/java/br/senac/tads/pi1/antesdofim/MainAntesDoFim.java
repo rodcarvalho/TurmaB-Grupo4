676bdb5570396b9146aedbac3241db795762ca24
rodcarvalho <barretocarvalho.contato@gmail.com>
2021-10-18 22:17:58 -0300

    Created Project - first commit

---
+++ AntesDoFim/src/main/java/br/senac/tads/pi1/antesdofim/MainAntesDoFim.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi1.antesdofim;
+
+/**
+ *
+ * @author rodrigocarvalho
+ */
+public class MainAntesDoFim {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Começou o PI");
+    }
+    
+}
