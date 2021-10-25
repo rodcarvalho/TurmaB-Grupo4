5229f6dc4deb297b81b258a200aacaeb16b13887
rodcarvalho <barretocarvalho.contato@gmail.com>
2021-10-25 19:38:22 -0300

    Created Main Menu

--- AntesDoFim/src/main/java/br/senac/tads/pi1/antesdofim/MainAntesDoFim.java
+++ AntesDoFim/src/main/java/br/senac/tads/pi1/antesdofim/MainAntesDoFim.java
@@ -5,6 +5,8 @@
  */
 package br.senac.tads.pi1.antesdofim;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rodrigocarvalho
@@ -15,7 +17,35 @@ public class MainAntesDoFim {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Começou o PI");
+        Scanner input = new Scanner(System.in);
+        int selectedOption;
+        boolean gameOn = true;
+        
+        while (gameOn) {
+            System.out.println("##### --- MENU INICIAL --- #####");
+            System.out.println(" 1. Instruções");
+            System.out.println(" 2. Jogar");
+            System.out.println(" 3. Créditos");
+            System.out.println(" 4. Sair");
+        
+            System.out.println("Digite o número da opção desejada: ");
+            selectedOption = input.nextInt();
+            
+            switch(selectedOption) {
+                case 1:
+                    System.out.println("SELECIONOU >>>>> Instruções");
+                    break;
+                case 2:
+                    System.out.println("SELECIONOU >>>>> Jogar");
+                    break;
+                case 3:
+                    System.out.println("Autor: Rodrigo B. Carvalho");
+                    break;
+                case 4: 
+                    System.out.println("Você saiu da aplicação. Até mais...");
+                    gameOn = false;
+                    break;
+            }
+        }
     }
-    
 }
